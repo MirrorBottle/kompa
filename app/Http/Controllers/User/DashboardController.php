0aba3bdfb43c165a344db8377eba142125c69d85
MirrorBottle <setiawanbayu66152@gmail.com>
2023-11-18 16:03:50 +0800

    feat: user dashboard chore: remove search

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,11 +3,55 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Salary;
+use App\Models\Sales;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\http;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('user/dashboard');
+
+    public function dashboard()
+    {
+        $salaries = Salary::where("user_id", auth()->user()->id)
+            ->orderBy('id', 'desc')
+            ->limit(3)
+            ->get();
+
+        $sales = Sales::where("user_id", auth()->user()->id)
+            ->orderBy('id', 'desc')
+            ->limit(3)
+            ->get();
+
+        return response()->json([
+            'status' => 'sucess',
+            'code' => 200,
+            'message' => 'Data dashboard berhasil diambil!',
+            'data' => [
+                'salaries' => $salaries->map(function ($salary) {
+                    return [
+                        'status'            => $salary->status_name_badge,
+                        'date' => $salary->start_date->format("d/m/y") . " ~ " .$salary->end_date->format("d/m/y"),
+                        'total'             => helperFormatCurrency($salary->base_salary + $salary->commission_amount)
+                    ];
+                }),
+                'sales' => $sales->map(function ($sale) {
+                    return [
+                        'date' => $sale->sale_date->format("d/m/Y"),
+                        'amount' => helperFormatCurrency($sale->sale_amount),
+                        'customer' => $sale->customer->name
+                    ];
+                })
+            ]
+        ]);
+    }
+    public function index()
+    {
+        $request = Request::create('/api/dashboard/user', 'GET');
+        $response = Route::dispatch($request);
+        $body = json_decode($response->getContent(), true);
+        $data = $body['data'];
+        return view('user/dashboard', compact('data'));
     }
 }
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,15 +1,74 @@
-
 @extends('layouts.admin.index')
-@section('title', 'Dashboard')
+@section('title', 'Selamat Datang, ' . auth()->user()->name)
 @section('content')
-    <div class="flex pt-16 overflow-hidden bg-gray-100">
-        <div id="main-content"
-            class="relative w-full max-w-screen-2xl mx-auto h-full overflow-y-auto bg-gray-100">
-            <main>
-                <div class="px-4 pt-6 2xl:px-0">
-                    
+    <section class="min-h-screen">
+        <div class="grid grid-cols-6 gap-6">
+            <div class="col-span-2">
+                <div
+                    class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex items-center justify-between mb-4">
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Penggajian</h5>
+                        <a href="{{ route('user.salary.index') }}"
+                            class="text-sm font-medium text-primary hover:underline dark:text-blue-500">
+                            Lihat Semua
+                        </a>
+                    </div>
+                    <div class="flow-root">
+                        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                            @foreach ($data['salaries'] as $salary)
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center">
+                                        <div class="flex-1 min-w-0">
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                {{ $salary['total'] }}
+                                            </p>
+                                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                {{ $salary['date'] }}
+                                            </p>
+                                        </div>
+                                        {!! $salary['status'] !!}
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </main>
+            </div>
+            <div class="col-span-4">
+                <div
+                    class="p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex items-center justify-between mb-4">
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Penjualan</h5>
+                        <a href="{{ route('user.sales.index') }}"
+                            class="text-sm font-medium text-primary hover:underline dark:text-blue-500">
+                            Lihat Semua
+                        </a>
+                    </div>
+                    <div class="flow-root">
+                        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                            @foreach ($data['sales'] as $sale)
+                                <li class="py-3 sm:py-4">
+                                    <div class="flex items-center">
+                                        <div class="flex-1 min-w-0">
+                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                {{ $sale['customer'] }}
+                                            </p>
+                                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                {{ $sale['date'] }}
+                                            </p>
+                                        </div>
+                                        <div
+                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                            {{ $sale['amount'] }}
+                                        </div>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
-    </div>
+    </section>
 @endsection
